3bb4ab5a2845afbedc57e0ddc0f378396ead0489
santelizvargas <ushija6@gmail.com>
2023-10-27 23:32:07 -0600

    Project added

---
+++ ThemeFactoryPattern/ThemeFactoryPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeFactoryPatternApp.swift
+//  ThemeFactoryPattern
+//
+//  Created by Steven Santeliz on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ThemeFactoryPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
